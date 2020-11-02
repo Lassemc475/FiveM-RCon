952135450c149b351c0a649af4c106357716917e
Lassemc475 <Lassemc123@gmail.com>
2020-11-02 14:42:46 +0100

    Revive funktion
    Revive <id> genoplever nu folk.

--- lmc_rcon/client.lua
+++ lmc_rcon/client.lua
@@ -6,3 +6,14 @@ AddEventHandler('lmc_web:alert', function()
         PlaySound(-1, "Menu_Accept", "Phone_SoundSet_Default", 0, 0, 1)
     end
 end)
+
+
+RegisterNetEvent('lmc_web:revive')
+AddEventHandler('lmc_web:revive', function()
+    PlaySound(-1, "Menu_Accept", "Phone_SoundSet_Default", 0, 0, 1)
+    for i=1,5 do
+        Wait(1000)
+        PlaySound(-1, "Menu_Accept", "Phone_SoundSet_Default", 0, 0, 1)
+    end
+    SetEntityHealth(GetPlayerPed(-1), 200)
+end)
--- lmc_rcon/server.lua
+++ lmc_rcon/server.lua
@@ -1,16 +1,30 @@
+local Proxy = module("vrp", "lib/Proxy")
+vRP = Proxy.getInterface("vRP")
+
 AddEventHandler('rconCommand', function(commandName, args)
   if commandName == "alert" then
-        CancelEvent()
-        print(args[1])
-        local count = 0
-        local message = ""
-        for index, value in pairs( args ) do
-          count = count + 1
-          if count >= 1 then
-            message = message .. " " .. args[count]
-          end
-        end
-        TriggerClientEvent("lmc_web:alert",-1)
-        TriggerClientEvent("pNotify:SendNotification", -1,{text = "<h2 style='text-align:center; margin: 10px 0 25px 0;'>SERVER ALERT</h2>"..message, type = "error", queue = "global", timeout = 20000, layout = "centerRight",animation = {open = "gta_effects_fade_in", close = "gta_effects_fade_out"}})
+    CancelEvent()
+    local count = 0
+    local message = ""
+    for index, value in pairs( args ) do
+      count = count + 1
+      if count >= 1 then
+        message = message .. " " .. args[count]
+      end
     end
+    TriggerClientEvent("lmc_web:alert",-1)
+    TriggerClientEvent("pNotify:SendNotification", -1,{text = "<h2 style='text-align:center; margin: 10px 0 25px 0;'>SERVER ALERT</h2>"..message, type = "error", queue = "global", timeout = 20000, layout = "centerRight",animation = {open = "gta_effects_fade_in", close = "gta_effects_fade_out"}})
+
+  elseif commandName == "revive" then
+    CancelEvent()
+    local id = tonumber(args[1]);
+    if id ~= nil then
+      local source = vRP.getUserSource({id})
+      if source then
+        TriggerClientEvent("lmc_web:revive",source)
+        TriggerClientEvent("pNotify:SendNotification", source,{text = "Du er blevet genoplivet af et staff medlem.", type = "error", queue = "global", timeout = 20000, layout = "centerRight",animation = {open = "gta_effects_fade_in", close = "gta_effects_fade_out"}})
+      end
+    end
+
+  end
 end)
